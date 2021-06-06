e503ae1eecf44852e96929c9a818c0d1dceaaab4
Will Palmquist <will.palmquist@du.edu>
2021-06-06 06:27:38 +0100

    tiingo_prep updated function

--- R/tiingo_prep.R
+++ R/tiingo_prep.R
@@ -1,12 +1,14 @@
-#' Title
+#' Prep fundamental or pricing data from Tiingo API
 #'
-#' @param data a data.frame of company financial statements retrieved from `riingo_fundamental_statements()`
+#' @param data a data.frame of company financial statements retrieved from `riingo_fundamental_statements()` or pricing data from `riingo_prices()`
 #'
 #' @return data.frame of combined quarterly fundamentals
 #' @export
 #'
 #' @examples
-merge_tiingo_fundamentals <- function(data){
+tiingo_prep <- function(data, #data from tiingo
+                        type = c("fundamental","price")){
+  if (type == "fundamental") {
   data <- data %>%
     filter(.,quarter != 0)
   df_all <- NULL
@@ -33,4 +35,10 @@ merge_tiingo_fundamentals <- function(data){
   df_all <- rbind(df_all[x,], df_all[!rownames(df)%in%x,])
   df_all <- df_all[!rownames(df_all) %in% c("ticker1","quarterly indicator1","quarter_end_date1"),]
   return(df_all)
+  }
+  else if (type == "price") {
+    data$date <- as.Date(data$date, "%Y/%m/%d")
+    data <- xts(data[, 3:ncol(data)], order.by = data$date)
+    return(data)
+  }
 }
